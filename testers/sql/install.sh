64610eb7a28b2cfca499d4e68123e01b94a03135
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-04-12 16:52:05 -0400

    sql: Update installation to work with specs file

--- testers/sql/install.sh
+++ testers/sql/install.sh
@@ -6,57 +6,62 @@ if [ $# -ne 1 ]; then
 fi
 
 SQLDIR=$1
+SPECS=${SQLDIR}/specs.json
 SOLUTIONDIR=${SQLDIR}/solution
 DATASETDIR=${SOLUTIONDIR}/datasets
 QUERYDIR=${SOLUTIONDIR}/queries
 SCHEMAFILE=${SOLUTIONDIR}/schema.ddl
-SERVERDB=ate_oracle
-SERVERUSER=ateserver
-SERVERPWD=YOUR_SERVER_PASSWORD
-TESTDB=ate_tests
-TESTUSER=atetest
-TESTPWD=YOUR_TEST_PASSWORD
+
+ORACLEUSER=ateserver
+ORACLEPWD=x
+ORACLEDB=$(jq -r '.oracle_database' ${SPECS})
+TESTPWD=$(jq -r '.user_password' ${SPECS})
+TESTUSER=$(jq -r '.user_name' ${SPECS})
+TESTDB=$(jq -r '.test_database' ${SPECS})
 
 echo "[SQL] Installing system packages"
 sudo apt-get install python3 postgresql
-echo "[SQL] Creating solution and test databases and users"
-chmod go-rwx ${QUERYDIR}
+echo "[SQL] Creating databases and users"
 sudo -u postgres psql <<-EOF
-	CREATE ROLE ${SERVERUSER} LOGIN PASSWORD '${SERVERPWD}';
+	CREATE ROLE ${ORACLEUSER} LOGIN PASSWORD '${ORACLEPWD}';
 	CREATE ROLE ${TESTUSER} LOGIN PASSWORD '${TESTPWD}';
-	DROP DATABASE IF EXISTS ${SERVERDB};
+	DROP DATABASE IF EXISTS ${ORACLEDB};
 	DROP DATABASE IF EXISTS ${TESTDB};
-	CREATE DATABASE ${SERVERDB} OWNER ${SERVERUSER};
+	CREATE DATABASE ${ORACLEDB} OWNER ${ORACLEUSER};
 	CREATE DATABASE ${TESTDB} OWNER ${TESTUSER};
-	\connect ${SERVERDB}
+	\connect ${ORACLEDB}
 	DROP SCHEMA IF EXISTS public CASCADE;
 	\connect ${TESTDB}
 	DROP SCHEMA IF EXISTS public CASCADE;
 EOF
-for datafile in ${DATASETDIR}/*; do
-	schemaname=$(basename -s .sql ${datafile})
-	echo "[SQL] Creating solution schema ${schemaname} and populating it"
-	psql -U ${SERVERUSER} -d ${SERVERDB} -h localhost <<-EOF
-		DROP SCHEMA IF EXISTS ${schemaname} CASCADE;
-		CREATE SCHEMA ${schemaname};
-		GRANT USAGE ON SCHEMA ${schemaname} TO ${TESTUSER};
-	EOF
-	echo "SET search_path TO ${schemaname};" | cat - ${SCHEMAFILE} >| /tmp/ate.sql
-	psql -U ${SERVERUSER} -d ${SERVERDB} -h localhost -f /tmp/ate.sql
-	echo "SET search_path TO ${schemaname};" | cat - ${datafile} >| /tmp/ate.sql
-	psql -U ${SERVERUSER} -d ${SERVERDB} -h localhost -f /tmp/ate.sql
-	for queryfile in ${QUERYDIR}/*; do
-		queryname=$(basename -s .sql ${queryfile})
-		if [[ ${schemaname} == ${queryname}* ]] || [[ ${schemaname} == all* ]]; then
-			echo "[SQL] Creating solution table ${queryname} for schema ${schemaname}"
-			echo "SET search_path TO ${schemaname};" | cat - ${queryfile} >| /tmp/ate.sql
-			psql -U ${SERVERUSER} -d ${SERVERDB} -h localhost -f /tmp/ate.sql
+echo "[SQL] Creating solutions"
+schemas=""
+chmod go-rwx ${QUERYDIR}
+jq -r '.matrix | keys[]' ${SPECS} | while read queryfile; do
+	queryname=$(basename -s .sql ${queryfile})
+	jq -r --arg q ${queryfile} '.matrix | .[$q] | keys | map(select(. != "extra"))[]' ${SPECS} | while read datafile; do
+		schemaname=$(basename -s .sql ${datafile})
+		echo "${schemas}"
+		if [[ "${schemas}" != *${schemaname}* ]]; then # first time using this dataset, create a schema for it
+			echo "[SQL] Creating schema for data '${schemaname}'"
+			psql -U ${ORACLEUSER} -d ${ORACLEDB} -h localhost <<-EOF
+				DROP SCHEMA IF EXISTS ${schemaname} CASCADE;
+				CREATE SCHEMA ${schemaname};
+				GRANT USAGE ON SCHEMA ${schemaname} TO ${TESTUSER};
+			EOF
+			echo "SET search_path TO ${schemaname};" | cat - ${SCHEMAFILE} ${DATASETDIR}/${datafile} >| /tmp/ate.sql
+			psql -U ${ORACLEUSER} -d ${ORACLEDB} -h localhost -f /tmp/ate.sql
+			schemas="${schemas} ${schemaname}"
 		fi
+		echo "${schemas}"
+		echo "[SQL] Creating solution '${queryname}' for data '${schemaname}'"
+		echo "SET search_path TO ${schemaname};" | cat - ${QUERYDIR}/${queryfile} >| /tmp/ate.sql
+		psql -U ${ORACLEUSER} -d ${ORACLEDB} -h localhost -f /tmp/ate.sql
+		psql -U ${ORACLEUSER} -d ${ORACLEDB} -h localhost <<-EOF
+			GRANT SELECT ON ${schemaname}.${queryname} TO ${TESTUSER};
+		EOF
 	done
-	psql -U ${SERVERUSER} -d ${SERVERDB} -h localhost <<-EOF
-		GRANT SELECT ON ALL TABLES IN SCHEMA ${schemaname} TO ${TESTUSER};
-	EOF
 done
 rm /tmp/ate.sql
-# TODO Should write settings to the config file
-#echo "[SQL] Updating python config file"
+echo '[SQL] Updating json specs file'
+sed -i -e "s#/path/to/solution#${SOLUTIONDIR}#g" ${SPECS}
