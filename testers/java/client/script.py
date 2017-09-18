6adb7fb860e078b723766bc8914d0ba94f5b4ab7
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-09-18 15:38:56 -0400

    java: Update to follow python specs simplification

--- testers/java/client/script.py
+++ testers/java/client/script.py
@@ -16,22 +16,25 @@ if __name__ == '__main__':
     assignment_id = sys.argv[3]
     group_id = sys.argv[4]
     repo_name = sys.argv[5]
-    TEST_SPECS = MarkusTestSpecs('/path/to/specs')
+    SPECS = MarkusTestSpecs()
 
-    # Modify uppercase variables with your settings
-
+    # The test files to run (precompiled on the server), and the points assigned:
+    # points can be assigned per test function, or per test class (every test function in the class will be worth those
+    # points); if a test function/class is missing, it is assigned a default of 1 point (use POINTS = {} for all 1s).
     POINTS1 = {'testPasses': 1, 'testFails': 2}
     POINTS2 = {'Test2': 1}
-    TEST_SPECS.set_test_points('Test1.java', POINTS1)
-    TEST_SPECS.set_test_points('Test2.java', POINTS2)
-    # The max time to run all tests on the student submission.
-    TEST_SPECS['global_timeout'] = 30
-    # The feedback file name
-    FEEDBACK_FILE = 'feedback_java.txt'
+    SPECS['test_points'] = {'Test1.java': POINTS1}
+    SPECS['test_points'] = {'Test2.java': POINTS2}
 
-    tester = MarkusJAMTester(specs=TEST_SPECS, feedback_file=FEEDBACK_FILE)
+    # The max time to run all tests (defaults to 30 seconds if commented out).
+    # SPECS['global_timeout'] = 30
+
+    # The feedback file name (defaults to no feedback file if commented out).
+    # SPECS['feedback_file'] = 'feedback_java.txt'
+
+    tester = MarkusJAMTester(specs=SPECS)
     tester.run()
-    # use markus apis if needed
+    # Use markus apis if needed
     # if os.path.isfile(FEEDBACK_FILE):
     #     api = Markus(api_key, root_url)
     #     with open(FEEDBACK_FILE) as feedback_open:
