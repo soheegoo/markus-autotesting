3e14d801890295d04b152da3afb0a7b7df05b703
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-09-19 17:52:17 -0400

    java: Update solution test timeout to 10 seconds rather than milliseconds

--- testers/java/solution/tests/Test1.java
+++ testers/java/solution/tests/Test1.java
@@ -7,13 +7,13 @@ public class Test1 {
 
     Submission submission = new Submission();
 
-    @Test(timeout=10)
+    @Test(timeout=10000)
     @Description(description="This test should pass")
     public void testPasses() {
         assertTrue(submission.returnTrue());
     }
 
-    @Test(timeout=10)
+    @Test(timeout=10000)
     @Description(description="This test should fail")
     public void testFails() {
         assertTrue(submission.returnFalse());
