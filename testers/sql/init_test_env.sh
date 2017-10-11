21021718bc8a3ba5bb4dd2e45cd0170334b7f9fe
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-10-11 17:12:51 -0400

    sql: Move db creation into install script

--- testers/sql/init_test_env.sh
+++ testers/sql/init_test_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 2 ]; then
-    echo usage: $0 autotest_working_dir specs_dir
+    echo "Usage: $0 autotest_working_dir specs_dir"
     exit 1
 fi
 
@@ -12,32 +12,14 @@ SPECSDIR=$(readlink -f $2)
 SPECS=${SPECSDIR}/specs.json
 ORACLEDB=$(jq -r '.oracle_database' ${SPECS})
 ORACLEUSER=${ORACLEDB}
-TESTDBS=( $(jq -r '.tests[] | .database' ${SPECS}) )
 TESTUSERS=( $(jq -r '.tests[] | .user' ${SPECS}) )
-TESTPWDS=( $(jq -r '.tests[] | .password' ${SPECS}) )
 SOLUTIONDIR=${SPECSDIR}/solution
 DATASETDIR=${SOLUTIONDIR}/datasets
 QUERYDIR=${SOLUTIONDIR}/queries
 SCHEMAFILE=${SOLUTIONDIR}/schema.ddl
 
-echo "[SQL] Creating databases and users (you'll be asked to create a password for the oracle user ${ORACLEUSER})"
-sudo -u postgres createuser -P ${ORACLEUSER} # secure, password is not logged
-sudo -u postgres psql <<-EOF
-	DROP DATABASE IF EXISTS ${ORACLEDB};
-	CREATE DATABASE ${ORACLEDB} OWNER ${ORACLEUSER};
-	\connect ${ORACLEDB}
-	DROP SCHEMA IF EXISTS public CASCADE;
-EOF
-for i in "${!TESTDBS[@]}"; do
-	sudo -u postgres psql <<-EOF
-		CREATE ROLE ${TESTUSERS[i]} LOGIN PASSWORD '${TESTPWDS[i]}';
-		DROP DATABASE IF EXISTS ${TESTDBS[i]};
-		CREATE DATABASE ${TESTDBS[i]} OWNER ${TESTUSERS[i]};
-		\connect ${TESTDBS[i]}
-		DROP SCHEMA IF EXISTS public CASCADE;
-	EOF
-done
 echo "[SQL] Loading solutions into the oracle database"
+ALLTESTUSERS=$(IFS=,; echo "${TESTUSERS[*]}")
 cp -a ${WORKINGDIR}/solution ${SPECSDIR}
 chmod go-rwx ${QUERYDIR}
 schemas=""
@@ -47,13 +29,12 @@ for queryfile in "${queries[@]}"; do
     datasets=( $(jq -r --arg q ${queryfile} '.matrix | .[$q] | keys | map(select(. != "extra"))[]' ${SPECS}) )
     for datafile in "${datasets[@]}"; do
         schemaname=$(basename -s .sql ${datafile})
-        users=$(IFS=,; echo "${TESTUSERS[*]}")
         if [[ "${schemas}" != *" ${schemaname} "* ]]; then # first time using this dataset, create a schema for it
             echo "[SQL] Creating schema for dataset '${schemaname}'"
             echo "
                 DROP SCHEMA IF EXISTS ${schemaname} CASCADE;
                 CREATE SCHEMA ${schemaname};
-                GRANT USAGE ON SCHEMA ${schemaname} TO ${users};
+                GRANT USAGE ON SCHEMA ${schemaname} TO ${ALLTESTUSERS};
                 SET search_path TO ${schemaname};
             " | cat - ${SCHEMAFILE} ${DATASETDIR}/${datafile} >| /tmp/ate.sql
             psql -U ${ORACLEUSER} -d ${ORACLEDB} -h localhost -f /tmp/ate.sql
@@ -61,7 +42,7 @@ for queryfile in "${queries[@]}"; do
         fi
         echo "[SQL] Creating solution table '${queryname}' for dataset '${schemaname}'"
         echo "SET search_path TO ${schemaname};" | cat - ${QUERYDIR}/${queryfile} >| /tmp/ate.sql
-        echo "GRANT SELECT ON ${schemaname}.${queryname} TO ${users};" >> /tmp/ate.sql
+        echo "GRANT SELECT ON ${schemaname}.${queryname} TO ${ALLTESTUSERS};" >> /tmp/ate.sql
         psql -U ${ORACLEUSER} -d ${ORACLEDB} -h localhost -f /tmp/ate.sql
     done
 done
--- testers/sql/install.sh
+++ testers/sql/install.sh
@@ -1,9 +1,65 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 0 ]; then
-	echo usage: $0
+if [[ $# -lt 2 || $# -gt 3 ]]; then
+	echo "Usage: $0 oracle_user test_user [num_users]"
 	exit 1
+fi
+
+THISSCRIPT=$(readlink -f ${BASH_SOURCE})
+TESTERDIR=$(dirname ${THISSCRIPT})
+ORACLEUSER=$1
+TESTUSER=$2
+ORACLEDB=${ORACLEUSER}
+TESTDBS=()
+TESTUSERS=()
+TESTPWDS=()
+if [ $# -eq 3 ]; then
+    NUMUSERS=$3
+	for i in $(seq 0 $((NUMUSERS - 1))); do
+	    TESTDBS[$i]="${TESTUSER}$i"
+	    TESTUSERS[$i]="${TESTUSER}$i"
+	done
+else
+    TESTDBS[0]=${TESTUSER}
+    TESTUSERS[0]=${TESTUSER}
 fi
 
 echo "[SQL] Installing system packages"
 sudo apt-get install python3 postgresql jq
+echo "[SQL] Creating databases and users"
+echo "[SQL] Creating oracle user '${ORACLEUSER}' with database '${ORACLEDB}'"
+sudo -u postgres psql <<-EOF
+	DROP DATABASE IF EXISTS ${ORACLEDB};
+	DROP ROLE IF EXISTS ${ORACLEUSER};
+EOF
+echo "[SQL] Create password for oracle user '${ORACLEUSER}'"
+sudo -u postgres createuser -P ${ORACLEUSER} # secure, password is not logged
+sudo -u postgres psql <<-EOF
+	CREATE DATABASE ${ORACLEDB} OWNER ${ORACLEUSER};
+	REVOKE CONNECT ON DATABASE ${ORACLEDB} FROM PUBLIC;
+	\connect ${ORACLEDB}
+	DROP SCHEMA IF EXISTS public CASCADE;
+EOF
+TESTS="  \"tests\": ["
+for i in "${!TESTDBS[@]}"; do
+    echo "[SQL] Creating test user '${TESTUSERS[$i]}' with database '${TESTDBS[$i]}'"
+    read -s -p "[SQL] Create password for test user '${TESTUSERS[$i]}': " TESTPWDS[$i]
+	sudo -u postgres psql <<-EOF
+		DROP DATABASE IF EXISTS ${TESTDBS[$i]};
+		DROP ROLE IF EXISTS ${TESTUSERS[$i]};
+		CREATE ROLE ${TESTUSERS[$i]} LOGIN PASSWORD '${TESTPWDS[$i]}';
+		CREATE DATABASE ${TESTDBS[$i]} OWNER ${TESTUSERS[$i]};
+		REVOKE CONNECT ON DATABASE ${TESTDBS[$i]} FROM PUBLIC;
+		GRANT CONNECT ON DATABASE ${ORACLEDB} TO ${TESTUSERS[$i]};
+		\connect ${TESTDBS[$i]}
+		DROP SCHEMA IF EXISTS public CASCADE;
+	EOF
+    if [ $i -ne 0 ]; then
+        TESTS="${TESTS},"
+    fi
+    TESTS="${TESTS}{\"database\": \"${TESTDBS[$i]}\", \"user\": \"${TESTUSERS[$i]}\", \"password\": \"${TESTPWDS[i]}\"}"
+done
+TESTS="${TESTS}],"
+echo '[SQL] Updating json specs file'
+sed -i -e "s#oracle_db#${ORACLEDB}#g" ${TESTERDIR}/specs.json
+sed -i -e "s#^  \"tests\".*\$#${TESTS}#g" ${TESTERDIR}/specs.json
