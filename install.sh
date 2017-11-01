c7db9181d7e5a55a7993115868a573daef4541a1
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-11-01 17:21:06 -0400

    server: Update installation and managing scripts

--- install.sh
+++ install.sh
@@ -1,108 +1,66 @@
 #!/usr/bin/env bash
 
-# Deploys the autotesting server and optionally the available testers.
-# (run this as a super user, e.g. sudo install.sh)
+# Deploys the autotesting server (run this as a super user, e.g. sudo install.sh)
 
-usage() {
-	echo
-	echo "**USAGE**"
-	echo
-	echo "$0 [-h] [-q QUEUE] [-s SERVER_USER] [-t TEST_USER] [-d WORKING_DIR] [-T TESTERS]"
-	echo
-	echo "**OPTIONS**"
-	echo
-	echo "-q QUEUE, --queue QUEUE: [optional, defaults to \"ate_tests\"] The name of the test server queue."
-	echo "-s SERVER_USER, --user-server SERVER_USER: [optional, defaults to \"ateserver\"] The user that runs the test server."
-	echo "-t TEST_USER, --user-test TEST_USER: [optional, defaults to \"atetest\"] The user that runs the student code being tested."
-	echo "-d WORKING_DIR, --dir WORKING_DIR: [optional, defaults to \"../\"] The name of the test server working directory."
-	echo "-T TESTERS, --testers TESTERS: [optional] A comma-separated list of tester names to install together with the test server."
-	echo "-h, --help: [optional] Prints this help."
-	echo
-	echo "**EXAMPLES**"
-	echo
-	echo "./install.sh"
-	echo "Installs the MarkUs autotester, using the queue \"ate_tests\", server user \"ateserver\" and test user \"atetest\"."
-	echo
-	echo "./install.sh -q some_queue -s server_user -t test_user -d /some/dir -T uam"
-	echo "Installs the MarkUs autotester, using the queue \"some_queue\", server user \"server_user\", test user \"test_user\", working directory \"/some/dir\", and the \"uam\" tester."
-}
+if [[ $# -lt 3 || $# -gt 4 ]]; then
+    echo "Usage: $0 server_user test_user working_dir [num_workers]"
+    exit 1
+fi
 
-SHORT=q:s:t:d:T:h
-LONG=queue:,user-server:,user-test:,dir:,testers:,help
-PARSED=`getopt -o ${SHORT} -l ${LONG} -n "$0" -- "$@"`
-eval set -- "${PARSED}"
-
-QUEUENAME=ate_tests
-USERSERVER=ateserver
-USERTEST=atetest
-WORKINGDIR=..
-TESTERS=()
-while true; do
-	case "$1" in
-		-q | --queue )
-			QUEUENAME="$2"
-			shift 2
-			;;
-		-s | --server-user )
-			USERSERVER="$2"
-			shift 2
-			;;
-		-t | --test-user )
-			USERTEST="$2"
-			shift 2
-			;;
-		-d | --dir )
-			WORKINGDIR="$2"
-			shift 2
-			;;
-		-T | --testers )
-			IFS=',' read -a TESTERS <<< "$2"
-			shift 2
-			;;
-		-h | --help )
-			usage
-			exit
-			;;
-		-- )
-			shift
-			break
-			;;
-		* )
-			usage >&2
-			exit 1
-	esac
-done
-
-SERVERDIR=server
-TESTERSDIR=testers
+THISSCRIPT=$(readlink -f ${BASH_SOURCE})
+THISSCRIPTDIR=$(dirname ${THISSCRIPT})
+SERVERUSER=$1
+TESTUSER=$2
+WORKINGDIR=$(readlink -f $3)
+NUMWORKERS=1
+if [[ $# -eq 4 ]]; then
+    NUMWORKERS=$4
+fi
+SERVERDIR=${THISSCRIPTDIR}/server
 SPECSDIR=${WORKINGDIR}/specs
 VENVSDIR=${WORKINGDIR}/venvs
-FILESDIR=${WORKINGDIR}/files
-TESTSDIR=${WORKINGDIR}/tests
-RESULTSDIR=${WORKINGDIR}/test_runs
+RESULTSDIR=${WORKINGDIR}/results
+QUEUE=${TESTUSER}
 
 echo "[AUTOTEST] Installing system packages"
-sudo apt-get install ruby bundler redis-server jq
-pushd ${SERVERDIR}
-echo "[AUTOTEST] Installing gems"
-bundle install --deployment
-sudo -u ${USERSERVER} ./start_resque.sh . ${QUEUENAME}
-echo "[AUTOTEST] (You may want to add the Resque commands to ${USERSERVER}'s crontab with a @reboot time)"
-popd
+apt-get install ruby bundler redis-server
+echo "[AUTOTEST] Creating server user '${SERVERUSER}'"
+adduser --disabled-password ${SERVERUSER}
+conf=""
+for i in $(seq 0 $((NUMWORKERS - 1))); do
+    testdir=${WORKINGDIR}/${TESTUSER}${i}
+    echo "[AUTOTEST] Creating test user '${TESTUSER}${i}'"
+    adduser --disabled-login --no-create-home ${TESTUSER}${i}
+    mkdir -p ${testdir}
+    chmod ug=rwx,o= ${testdir}
+    chown ${TESTUSER}${i}:${SERVERUSER} ${testdir}
+    echo "${SERVERUSER} ALL=(${TESTUSER}${i}) NOPASSWD:ALL" | EDITOR="tee -a" visudo
+    conf="${conf}{user: '${TESTUSER}${i}', dir: '${testdir}', queue: '${QUEUE}${i}'},"
+done
+echo "[AUTOTEST] Creating working directories"
 mkdir -p ${SPECSDIR}
 mkdir -p ${VENVSDIR}
-mkdir -p ${FILESDIR}
-mkdir -p ${TESTSDIR}
-chmod ug=rwx,o= ${TESTSDIR}
 mkdir -p ${RESULTSDIR}
 chmod u=rwx,go= ${RESULTSDIR}
-chown ${USERSERVER}:${USERSERVER} ${SPECSDIR} ${VENVSDIR} ${FILESDIR} ${RESULTSDIR}
-chown ${USERTEST}:${USERSERVER} ${TESTSDIR}
-for i in "${!TESTERS[@]}"; do
-	TESTERNAME=${TESTERS[$i]}
-	if cd ${TESTERSDIR}/${TESTERNAME}; then
-		echo "[AUTOTEST] Installing tester ${TESTERNAME}"
-		./install.sh $(pwd)
-		cd ../../
-	fi
-done
+chown ${SERVERUSER}:${SERVERUSER} ${SPECSDIR} ${VENVSDIR} ${RESULTSDIR}
+echo "[AUTOTEST] Installing gems"
+pushd ${SERVERDIR} > /dev/null
+bundle install --deployment
+sudo -u ${SERVERUSER} -- ./start_resque.sh ${QUEUE} ${NUMWORKERS}
+popd > /dev/null
+echo "[AUTOTEST] Creating Markus web server config snippet in 'markus_conf.rb'"
+echo "
+    AUTOMATED_TESTING_ENGINE_ON = true
+    ATE_EXPERIMENTAL_STUDENT_TESTS_ON = false
+    ATE_EXPERIMENTAL_STUDENT_TESTS_BUFFER_TIME = 2.hours
+    ATE_CLIENT_DIR = 'TODO_web_server_dir'
+    ATE_FILES_QUEUE_NAME = 'TODO_web_server_queue'
+    ATE_SERVER_HOST = '$(hostname).$(dnsdomainname)'
+    ATE_SERVER_FILES_USERNAME = '${SERVERUSER}'
+    ATE_SERVER_FILES_DIR = '${WORKINGDIR}'
+    ATE_SERVER_RESULTS_DIR = '${RESULTSDIR}'
+    ATE_SERVER_TESTS = [${conf}]
+" >| markus_conf.rb
+echo "[AUTOTEST] (You must add the Markus web server user's public key to '$(~${SERVERUSER})/.ssh/authorized_keys')"
+echo "[AUTOTEST] (You may want to add '${SERVERDIR}/start_rescue.sh ${QUEUE} ${NUMWORKERS}' to ${SERVERUSER}'s crontab with a @reboot time)"
+echo "[AUTOTEST] (You should install the individual testers you plan to use)"
--- testers/create_test_env.sh
+++ testers/create_test_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $# -lt 4 || $# -gt 5 ]]; then
-    echo usage: $0 autotest_working_dir tester_name python_version course_name env_name
+    echo "Usage: $0 autotest_working_dir tester_name python_version course_name env_name"
     exit 1
 fi
 
@@ -13,7 +13,7 @@ PYVERSION=$3
 COURSENAME=$4
 SPECSDIR=${WORKINGDIR}/specs/${COURSENAME}
 VENVDIR=${WORKINGDIR}/venvs/${COURSENAME}
-if [ $# -eq 5 ]; then
+if [[ $# -eq 5 ]]; then
     ENVNAME=$5
     SPECSDIR=${SPECSDIR}/${ENVNAME}
     VENVDIR=${VENVDIR}/${ENVNAME}
@@ -23,7 +23,7 @@ TESTERDIR=${THISSCRIPTDIR}/${TESTERNAME}
 echo "[ENV] Installing system packages"
 sudo apt-get install python3 python3-venv
 if pushd ${TESTERDIR} > /dev/null; then
-    echo "[ENV] Creating specs directory ${SPECSDIR}"
+    echo "[ENV] Creating specs directory '${SPECSDIR}'"
     mkdir -p ${SPECSDIR}
     if [[ -e ${WORKINGDIR}/specs.json ]]; then
         cp ${WORKINGDIR}/specs.json ${SPECSDIR}
@@ -33,17 +33,18 @@ if pushd ${TESTERDIR} > /dev/null; then
     if [[ -e init_test_env.sh ]]; then
         ./init_test_env.sh ${WORKINGDIR} ${SPECSDIR}
     fi
-    echo "[ENV] Creating virtualenv ${VENVDIR}"
+    echo "[ENV] Creating virtualenv '${VENVDIR}'"
     python${PYVERSION} -m venv ${VENVDIR}
     source ${VENVDIR}/bin/activate
     pip install wheel
-    if [ -e requirements.txt ]; then
+    if [[ -e requirements.txt ]]; then
         pip install -r requirements.txt
     fi
     echo "$(pwd)/server" > ${VENVDIR}/lib/python${PYVERSION}/site-packages/markus_${TESTERNAME}.pth
     cd ..
     echo "$(pwd)" > ${VENVDIR}/lib/python${PYVERSION}/site-packages/markus.pth
     popd > /dev/null
+    echo "[ENV] (You should use this shebang in the test scripts: '#!${VENVDIR}/bin/python3')"
 else
-    echo "[ENV] The tester ${TESTERNAME} does not exist"
+    echo "[ENV] The tester '${TESTERNAME}' does not exist"
 fi
