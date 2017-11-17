520c77edaa762c3411b1933d3dd058e64aef4501
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-11-17 18:19:03 -0500

    server: Refactor main scripts using functions

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+install_packages() {
+    echo "[AUTOTEST] Installing system packages"
+    sudo apt-get install ruby redis-server
+    sudo gem install bundler
+}
+
+create_server_user() {
+    echo
+    if id ${SERVERUSER} > /dev/null 2>&1; then
+        echo "[AUTOTEST] Reusing existing server user '${SERVERUSER}'"
+    else
+        echo "[AUTOTEST] Creating server user '${SERVERUSER}'"
+        sudo adduser --disabled-password ${SERVERUSER}
+    fi
+    sudo mkdir -p ${WORKINGDIR}
+    sudo chown ${SERVERUSER}:${SERVERUSER} ${WORKINGDIR}
+}
+
 create_test_user() {
     local testuser=$1
     local queue=$2
@@ -18,67 +36,84 @@ create_test_user() {
     CONF="${CONF}{user: '${testuser}', dir: '${testdir}', queue: '${queue}'},"
 }
 
+create_test_users() {
+    if [[ -z ${NUMWORKERS} ]]; then
+        create_test_user ${TESTUSER} ${QUEUE}
+    else
+        for i in $(seq 0 $((NUMWORKERS - 1))); do
+            create_test_user ${TESTUSER}${i} ${QUEUE}${i}
+        done
+    fi
+}
+
+create_working_dirs() {
+    echo "[AUTOTEST] Creating working directories"
+    sudo mkdir ${SPECSDIR}
+    sudo mkdir ${VENVSDIR}
+    sudo mkdir ${RESULTSDIR}
+    sudo chown ${SERVERUSER}:${SERVERUSER} ${SPECSDIR} ${VENVSDIR} ${RESULTSDIR}
+    sudo chmod u=rwx,go= ${RESULTSDIR}
+}
+
+install_gems() {
+    echo "[AUTOTEST] Installing gems"
+    pushd ${SERVERDIR} > /dev/null
+    bundle install --deployment
+    popd > /dev/null
+}
+
+create_markus_conf() {
+    echo "[AUTOTEST] Creating Markus web server config snippet in 'markus_conf.rb'"
+    echo "
+        AUTOMATED_TESTING_ENGINE_ON = true
+        ATE_EXPERIMENTAL_STUDENT_TESTS_ON = false
+        ATE_EXPERIMENTAL_STUDENT_TESTS_BUFFER_TIME = 2.hours
+        ATE_CLIENT_DIR = 'TODO_web_server_dir'
+        ATE_FILES_QUEUE_NAME = 'TODO_web_server_queue'
+        ATE_SERVER_HOST = '$(hostname).$(dnsdomainname)'
+        ATE_SERVER_FILES_USERNAME = '${SERVERUSER}'
+        ATE_SERVER_FILES_DIR = '${WORKINGDIR}'
+        ATE_SERVER_RESULTS_DIR = '${RESULTSDIR}'
+        ATE_SERVER_TESTS = [${CONF}]
+    " >| markus_conf.rb
+}
+
+suggest_next_steps() {
+    echo "[AUTOTEST] (You must add the Markus web server public key to ${SERVERUSER}'s '~/.ssh/authorized_keys')"
+    echo "[AUTOTEST] (You may want to add '${SERVERDIR}/start_resque.sh ${QUEUE} ${NUMWORKERS}' to ${SERVERUSER}'s crontab with a @reboot time)"
+    echo "[AUTOTEST] (You should install the individual testers you plan to use)"
+}
+
+# script starts here
 if [[ $# -lt 3 || $# -gt 4 ]]; then
     echo "Usage: $0 server_user test_user working_dir [num_workers]"
     exit 1
 fi
 
+# vars
 THISSCRIPT=$(readlink -f ${BASH_SOURCE})
 THISSCRIPTDIR=$(dirname ${THISSCRIPT})
 SERVERUSER=$1
 TESTUSER=$2
 WORKINGDIR=$(readlink -f $3)
+if [[ $# -eq 4 ]]; then
+    NUMWORKERS=$4
+else
+    NUMWORKERS=""
+fi
 SERVERDIR=${THISSCRIPTDIR}/server
 SPECSDIR=${WORKINGDIR}/specs
 VENVSDIR=${WORKINGDIR}/venvs
 RESULTSDIR=${WORKINGDIR}/results
 QUEUE=${TESTUSER}
-
-echo "[AUTOTEST] Installing system packages"
-sudo apt-get install ruby redis-server
-sudo gem install bundler
-if id ${SERVERUSER} > /dev/null 2>&1; then
-    echo "[AUTOTEST] Reusing existing server user '${SERVERUSER}'"
-else
-    echo "[AUTOTEST] Creating server user '${SERVERUSER}'"
-    sudo adduser --disabled-password ${SERVERUSER}
-fi
-sudo mkdir -p ${WORKINGDIR}
-sudo chown ${SERVERUSER}:${SERVERUSER} ${WORKINGDIR}
 CONF=""
-if [[ $# -eq 4 ]]; then
-    NUMWORKERS=$4
-    for i in $(seq 0 $((NUMWORKERS - 1))); do
-        create_test_user ${TESTUSER}${i} ${QUEUE}${i}
-    done
-else
-    NUMWORKERS=""
-    create_test_user ${TESTUSER} ${QUEUE}
-fi
-echo "[AUTOTEST] Creating working directories"
-sudo mkdir ${SPECSDIR}
-sudo mkdir ${VENVSDIR}
-sudo mkdir ${RESULTSDIR}
-sudo chown ${SERVERUSER}:${SERVERUSER} ${SPECSDIR} ${VENVSDIR} ${RESULTSDIR}
-sudo chmod u=rwx,go= ${RESULTSDIR}
-echo "[AUTOTEST] Installing gems"
-pushd ${SERVERDIR} > /dev/null
-bundle install --deployment
-sudo -u ${SERVERUSER} -- ./start_resque.sh ${QUEUE} ${NUMWORKERS}
-popd > /dev/null
-echo "[AUTOTEST] Creating Markus web server config snippet in 'markus_conf.rb'"
-echo "
-    AUTOMATED_TESTING_ENGINE_ON = true
-    ATE_EXPERIMENTAL_STUDENT_TESTS_ON = false
-    ATE_EXPERIMENTAL_STUDENT_TESTS_BUFFER_TIME = 2.hours
-    ATE_CLIENT_DIR = 'TODO_web_server_dir'
-    ATE_FILES_QUEUE_NAME = 'TODO_web_server_queue'
-    ATE_SERVER_HOST = '$(hostname).$(dnsdomainname)'
-    ATE_SERVER_FILES_USERNAME = '${SERVERUSER}'
-    ATE_SERVER_FILES_DIR = '${WORKINGDIR}'
-    ATE_SERVER_RESULTS_DIR = '${RESULTSDIR}'
-    ATE_SERVER_TESTS = [${CONF}]
-" >| markus_conf.rb
-echo "[AUTOTEST] (You must add the Markus web server public key to ${SERVERUSER}'s '~/.ssh/authorized_keys')"
-echo "[AUTOTEST] (You may want to add '${SERVERDIR}/start_resque.sh ${QUEUE} ${NUMWORKERS}' to ${SERVERUSER}'s crontab with a @reboot time)"
-echo "[AUTOTEST] (You should install the individual testers you plan to use)"
+
+# main
+install_packages
+create_server_user
+create_test_users
+create_working_dirs
+install_gems
+sudo -u ${SERVERUSER} -- ${SERVERDIR}/start_resque.sh ${QUEUE} ${NUMWORKERS}
+create_markus_conf
+suggest_next_steps
--- server/start_resque.sh
+++ server/start_resque.sh
@@ -1,30 +1,46 @@
 #!/usr/bin/env bash
 
+kill_workers() {
+    echo "[RESQUE] Killing existing Resque workers"
+    kill -QUIT `pgrep -f resque`
+}
+
 run_worker() {
     local queue=$1
 
     TERM_CHILD=1 BACKGROUND=yes QUEUES=${queue} bundle exec rake resque:work
     echo "[RESQUE] Resque worker listening on queue '${queue}'"
 }
+
+run_workers() {
+    echo "[RESQUE] Starting new Resque workers"
+    pushd ${THISSCRIPTDIR} > /dev/null
+    if [[ -z ${NUMWORKERS} ]]; then
+        run_worker ${QUEUE}
+    else
+        for i in $(seq 0 $((NUMWORKERS - 1))); do
+            run_worker ${QUEUE}${i}
+        done
+    fi
+    popd > /dev/null
+}
+
+# script starts here
 if [[ $# -lt 1 || $# -gt 2 ]]; then
     echo "Usage: $0 queue_name [num_workers]"
     exit 1
 fi
 
+# vars
 THISSCRIPT=$(readlink -f ${BASH_SOURCE})
 THISSCRIPTDIR=$(dirname ${THISSCRIPT})
 QUEUE=$1
-
-echo "[RESQUE] Killing existing Resque workers"
-kill -QUIT `pgrep -f resque`
-echo "[RESQUE] Starting new Resque workers"
-pushd ${THISSCRIPTDIR} > /dev/null
 if [[ $# -eq 2 ]]; then
     NUMWORKERS=$2
-    for i in $(seq 0 $((NUMWORKERS - 1))); do
-        run_worker ${QUEUE}${i}
-    done
 else
-    run_worker ${QUEUE}
+    NUMWORKERS=""
 fi
-popd > /dev/null
+
+# main
+kill_workers
+run_workers
