c90b1c1a292d9ee06719fe193db291a0d37278a9
Alessio Di Sandro <alessio.disandro@gmail.com>
2018-01-26 11:59:48 -0500

    java: Refactor installation scripts
     * Copy changes to python too

--- testers/java/init_specs.sh
+++ testers/java/init_specs.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
 
+compile_solution() {
+    local jamdir=${TESTERDIR}/uam-git/jam
+
+    echo "[JAVA] Compiling solution"
+    cp -a ${WORKINGDIR}/solution ${SPECSDIR}
+    pushd ${jamdir} > /dev/null
+    ./compile_tests.sh ${TESTSDIR} ${SOLUTIONDIR}
+    popd > /dev/null
+    rm -f ${SOLUTIONDIR}/*.java ${TESTSDIR}/*.java
+}
+
+update_specs() {
+    echo "[JAVA] Updating json specs file"
+    sed -i -e "s#/path/to/tests#${TESTSDIR}#g" ${SPECSDIR}/specs.json
+}
+
+# script starts here
 if [ $# -ne 2 ]; then
 	echo usage: $0 autotest_working_dir specs_dir
 	exit 1
 fi
 
+# vars
 THISSCRIPT=$(readlink -f ${BASH_SOURCE})
 TESTERDIR=$(dirname ${THISSCRIPT})
 WORKINGDIR=$(readlink -f $1)
 SPECSDIR=$(readlink -f $2)
-JAMDIR=${TESTERDIR}/uam-git/jam
 SOLUTIONDIR=${SPECSDIR}/solution
 TESTSDIR=${SOLUTIONDIR}/tests
 
-echo "[JAVA] Compiling solution"
-cp -a ${WORKINGDIR}/solution ${SPECSDIR}
-pushd ${JAMDIR} > /dev/null
-./compile_tests.sh ${TESTSDIR} ${SOLUTIONDIR}
-popd > /dev/null
-rm -f ${SOLUTIONDIR}/*.java ${TESTSDIR}/*.java
-echo "[JAVA] Updating json specs file"
-sed -i -e "s#/path/to/tests#${TESTSDIR}#g" ${SPECSDIR}/specs.json
+# main
+compile_solution
+update_specs
--- testers/java/install.sh
+++ testers/java/install.sh
@@ -1,31 +1,51 @@
 #!/usr/bin/env bash
 
+install_packages() {
+    echo "[JAVA] Installing system packages"
+    sudo apt-get install python3 openjdk-8-jre
+}
+
+download_uam() {
+    echo "[JAVA] Downloading latest version of UAM"
+    if pushd ${UAMDIR} &> /dev/null; then
+        git pull
+        popd > /dev/null
+    else
+        git clone https://github.com/ProjectAT/uam.git ${UAMDIR}
+    fi
+    if [[ ! -e ${UAMLINK} ]]; then
+        ln -s ${UAMDIR} ${UAMLINK}
+    fi
+}
+
+compile_jam() {
+    local jamdir=${UAMLINK}/jam
+
+    echo "[JAVA] Compiling JAM"
+    pushd ${jamdir} > /dev/null
+    ./compile_jam.sh
+    popd > /dev/null
+}
+
+update_specs() {
+    echo "[JAVA] Updating json specs file"
+    sed -i -e "s#/path/to/uam#${UAMLINK}#g" ${TESTERDIR}/specs.json
+}
+
+# script starts here
 if [ $# -ne 1 ]; then
-	echo usage: $0 uam_dir
+	echo "Usage: $0 uam_dir"
 	exit 1
 fi
 
+# vars
 THISSCRIPT=$(readlink -f ${BASH_SOURCE})
 TESTERDIR=$(dirname ${THISSCRIPT})
 UAMDIR=$(readlink -f $1)
 UAMLINK=${TESTERDIR}/uam-git
-JAMDIR=${UAMLINK}/jam
 
-echo "[JAVA] Installing system packages"
-sudo apt-get install python3 openjdk-8-jre
-echo "[JAVA] Downloading latest version of UAM"
-if pushd ${UAMDIR} &> /dev/null; then
-	git pull
-	popd > /dev/null
-else
-	git clone https://github.com/ProjectAT/uam.git ${UAMDIR}
-fi
-if [[ ! -e ${UAMLINK} ]]; then
-    ln -s ${UAMDIR} ${UAMLINK}
-fi
-echo "[JAVA] Compiling JAM"
-pushd ${JAMDIR} > /dev/null
-./compile_jam.sh
-popd > /dev/null
-echo "[JAVA] Updating json specs file"
-sed -i -e "s#/path/to/uam#${UAMLINK}#g" ${TESTERDIR}/specs.json
+# main
+install_packages
+download_uam
+compile_jam
+update_specs
