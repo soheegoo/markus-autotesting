94c24e430ee4c1a9baad99347b2300be8b0b0e11
Alessio Di Sandro <alessio.disandro@gmail.com>
2018-01-30 16:36:35 -0500

    java: Implement new junit5-based tester part 2

--- testers/java/server/MarkusJavaTester.java
+++ testers/java/server/MarkusJavaTester.java
@@ -1,2 +1,33 @@
+import org.junit.platform.engine.discovery.ClassNameFilter;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MarkusJavaTester {
+
+    public static void main(String[] args) {
+
+        Set<Path> classpath = new HashSet<>();
+        classpath.add(Paths.get("."));
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+            .selectors(DiscoverySelectors.selectClasspathRoots(classpath))
+            .filters(ClassNameFilter.includeClassNamePatterns("Test.*"))
+            .build();
+        Launcher launcher = LauncherFactory.create();
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+        listener.getSummary().printTo(new PrintWriter(System.out));
+    }
+
 }
