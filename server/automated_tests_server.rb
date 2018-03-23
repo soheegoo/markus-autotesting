ac63afc328f6b9a606dca8a149f51d4b20405643
Misha Schwartz <misha.schwartz@gmail.com>
2018-03-23 11:09:53 -0400

    xml format: ensure that full test xml is read even if the test hits the timeout limit and is killed

--- server/automated_tests_server.rb
+++ server/automated_tests_server.rb
@@ -48,7 +48,7 @@ class AutomatedTestsServer
           stdin.close
           # mimic capture3 to read safely and capture each line as it comes so that
           # these threads don't hang or raise an error if we have to kill them early
-          stdout_thread = Thread.new { stdout.each { |line| output << "#{line}\n" } }
+          stdout_thread = Thread.new { stdout.each_line('</test>') { |line| output << "#{line}" } }
           stderr_thread = Thread.new { stderr.each { |line| errors << "#{line}\n" } }
           if !thread.join(script['timeout']) # still running, let's kill the process group
             if test_username.nil?
