5bf2ae2a4c56776df4d951714e5a1622d68fb426
Misha Schwartz <misha.schwartz@gmail.com>
2018-05-28 10:45:31 -0400

    rq: use rq and supervisor instead of resque to create and monitor job queues (WIP)

--- install.sh
+++ install.sh
@@ -2,7 +2,30 @@
 
 install_packages() {
     echo "[AUTOTEST] Installing system packages"
-    sudo apt-get install ruby redis-server bundler
+    sudo apt-get install redis-server python3 python3-venv 
+}
+
+run_as_serveruser() {
+    local cmd=$1
+    if [[ -n ${SERVERUSER} ]]; then
+        cmd="sudo -u ${SERVERUSER} --set-home -- ${cmd}"
+    fi
+    ${cmd}
+}
+
+install_venv() {
+    local serverhome=$(echo ~${SERVERUSER})
+    echo "[AUTOTEST] Installing python3 virtual environment in $serverhome/autotstenv"
+    sudo python3 -m autotstenv $serverhome
+    sudo echo "source $serverhome/autotstenv/bin/activate" >> $serverhome/.bash_profile
+    echo "[AUTOTEST] Installing pip packages"
+    sudo source $serverhome/autotstenv/bin/activate
+    pip install redis rq
+    pip install git+https://github.com/Supervisor/supervisor@master
+    source deactivate
+    echo "[AUTOTEST] Make autotest_enqueuer.py executable by ${SERVERUSER}"
+    sudo echo 'export PATH=$PATH:'${SERVERDIR} >> $serverhome/.bash_profile
+    sudo echo 'test -f $HOME/.bash_profile && source $HOME/.bash_profile' >> $serverhome/.bashrc
 }
 
 create_server_user() {
@@ -21,6 +44,12 @@ create_server_user() {
     fi
 }
 
+add_user_info_to_redis() {
+    local json=$(printf '{"username":"%s", "working_dir":"%s"}' $1 $2)
+    local userlist=$(get_config_param USER_LIST)
+    redis-cli RPUSH userlist json > /dev/null
+}
+
 create_default_tester_dir() {
     local testerdir=${WORKINGDIR}/${QUEUE}
 
@@ -28,7 +57,7 @@ create_default_tester_dir() {
     sudo mkdir -p ${testerdir}
     sudo chown ${SERVERUSER2}:${SERVERUSER2} ${testerdir}
     sudo chmod ug=rwx,o=,+t ${testerdir}
-    CONF="${CONF}{user: nil, dir: '${testerdir}', queue: '${QUEUE}'},"
+    add_user_info_to_redis ${SERVERUSER2} ${testerdir}
 }
 
 create_tester_user() {
@@ -45,7 +74,7 @@ create_tester_user() {
     sudo chown ${SERVERUSER2}:${testeruser} ${testerdir}
     sudo chmod ug=rwx,o=,+t ${testerdir}
     echo "${SERVERUSER2} ALL=(${testeruser}) NOPASSWD:ALL" | sudo EDITOR="tee -a" visudo
-    CONF="${CONF}{user: '${testeruser}', dir: '${testerdir}', queue: '${testeruser}'},"
+    add_user_info_to_redis ${testeruser} ${testerdir}
 }
 
 create_tester_users() {
@@ -67,24 +96,16 @@ create_working_dirs() {
     sudo mkdir -p ${SPECSDIR}
     sudo mkdir -p ${VENVSDIR}
     sudo mkdir -p ${RESULTSDIR}
+    sudo mkdir -p ${SCRIPTSDIR}
     sudo chmod u=rwx,go= ${RESULTSDIR}
-    sudo chown ${SERVERUSER2}:${SERVERUSER2} ${SPECSDIR} ${VENVSDIR} ${RESULTSDIR}
+    sudo chown ${SERVERUSER2}:${SERVERUSER2} ${SPECSDIR} ${VENVSDIR} ${RESULTSDIR} ${SCRIPTSDIR}
 }
 
-install_gems() {
-    echo "[AUTOTEST] Installing gems"
-    pushd ${SERVERDIR} > /dev/null
-    bundle install --deployment
-    popd > /dev/null
-}
-
-run_resque() {
-    local cmd="${SERVERDIR}/start_resque.sh ${QUEUE} ${NUMWORKERS}"
-    if [[ -n ${SERVERUSER} ]]; then
-        cmd="sudo -u ${SERVERUSER} -- ${cmd}"
-    fi
-
-    ${cmd}
+start_queues() {
+    echo "[AUTOTEST] Generating generate_supervisord.conf file in ${THISSCRIPTDIR}"
+    run_as_serveruser "python generate_supervisord.conf.py" 
+    echo "[AUTOTEST] Starting rq workers using supervisor"
+    supervisord -c ${THISSCRIPTDIR}/supervisord.conf
 }
 
 create_markus_conf() {
@@ -106,7 +127,6 @@ create_markus_conf() {
         AUTOTEST_SERVER_FILES_USERNAME = ${serverconf}
         AUTOTEST_SERVER_FILES_DIR = '${WORKINGDIR}'
         AUTOTEST_SERVER_RESULTS_DIR = '${RESULTSDIR}'
-        AUTOTEST_SERVER_TESTS = [${CONF}]
     " >| markus_conf.rb
 }
 
@@ -114,12 +134,16 @@ suggest_next_steps() {
     if [[ -n ${SERVERUSER} ]]; then
         echo "[AUTOTEST] (You must add MarkUs web server's public key to ${SERVERUSER}'s '~/.ssh/authorized_keys')"
     fi
-    echo "[AUTOTEST] (You may want to add '${SERVERDIR}/start_resque.sh ${QUEUE} ${NUMWORKERS}' to ${SERVERUSER2}'s crontab with a @reboot time)"
+    echo "[AUTOTEST] (You may want to add '${SERVERDIR}/start_queues.sh ${QUEUE} ${NUMWORKERS}' to ${SERVERUSER2}'s crontab with a @reboot time)"
     echo "[AUTOTEST] (You should install the individual testers you plan to use)"
 }
 
 print_usage() {
-    echo "Usage: $0 working_dir [-s|--server <server_user>] [-t|--tester <tester_user>] [-w|--workers <num_workers>] [-h|--help]"
+    echo "Usage: $0 config_file [-s|--server <server_user>] [-t|--tester <tester_user>] [-w|--workers <num_workers>] [-h|--help]"
+}
+
+get_config_param() {
+    grep -Po "^$1\s*=\s*([\'\"])\K.*(?=\1)" ${CONFIGFILE}
 }
 
 # script starts here
@@ -139,7 +163,7 @@ THISUSER=$(whoami)
 NUMWORKERS=""
 SERVERUSER=""
 TESTERUSER=""
-QUEUE=tester # default queue, otherwise QUEUE == TESTERUSER
+CONFIGFILE=""
 while true; do
     case "$1" in
         -s | --server )
@@ -149,7 +173,6 @@ while true; do
         -t | --tester )
             if [[ $2 != ${SERVERUSER} ]]; then
                 TESTERUSER=$2
-                QUEUE=${TESTERUSER}
             fi
             shift 2
             ;;
@@ -172,28 +195,32 @@ while true; do
             exit 1
     esac
 done
-if [[ $# -ne 1 ]]; then
+if [[ $# -ne 2 ]]; then
     print_usage
     exit 1
 fi
-WORKINGDIR=$(readlink -f $1)
+
+CONFIGFILE=$(readlink -f $1)
+
 if [[ -n ${SERVERUSER} ]]; then
     SERVERUSER2=${SERVERUSER}
 else
     SERVERUSER2=${THISUSER}
 fi
+
+WORKINGDIR=$(get_config_param WORKING_DIR)
 SERVERDIR=${THISSCRIPTDIR}/server
-SPECSDIR=${WORKINGDIR}/specs
-VENVSDIR=${WORKINGDIR}/venvs
-RESULTSDIR=${WORKINGDIR}/results
-CONF=""
+SPECSDIR=${WORKINGDIR}/$(get_config_param SPECS_DIR_NAME)
+VENVSDIR=${WORKINGDIR}/$(get_config_param VENVS_DIR_NAME)
+RESULTSDIR=${WORKINGDIR}/$(get_config_param TEST_RESULT_DIR_NAME)
+SCRIPTSDIR=${WORKINGDIR}/$(get_config_param TEST_SCRIPT_DIR_NAME)
 
 # main
 install_packages
 create_server_user
 create_tester_users
+install_venv
 create_working_dirs
-install_gems
-run_resque
+start_queues
 create_markus_conf
 suggest_next_steps
---
+++ server/autotest_enqueuer.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import rq
+import json
+import inspect
+import autotest_server as ats
+sys.path.append(os.path.dirname(__file__))
+import config
+
+### HELPER FUNCTIONS ###
+
+def _job_id(markus_address, run_id, **kw):
+    """
+    Return a unique job id for each enqueued job
+    based on the markus_address and the run_id
+    """
+    return '{}_{}'.format(markus_address, run_id)
+
+def _check_args(func, *args, **kwargs):
+    """
+    Raises an error if calling the function func with args and
+    kwargs would raise an error.
+    """
+    inspect.signature(func).bind(*args, **kwargs)
+
+def _queue_name(queue, i):
+    """
+    Return a formatted queue name from a queue object and an integer
+    or some other unique identifier.
+    """
+    return '{}{}'.format(queue.type, i)
+
+def _get_queue(**kw):
+    """
+    Return a queue. The returned queue is one whose condition function
+    returns True when called with the arguments in **kw. 
+    """
+    condition, name = 0, 1
+    for queue_type in config.QUEUES:
+        if queue_type[condition](**kw):
+            return rq.Queue(queue_type[name], connection=ats._redis_connection())
+    raise RuntimeError('cannot enqueue job: unable to determine correct queue type') 
+
+def _print_queue_info(queue):
+    """
+    Print two strings to stdout indicating the number of jobs in the queue and the 
+    average pop interval during the current burst of jobs.
+    """
+    print('count :', queue.count)
+    print('pop_rate :', ats.get_avg_pop_interval(queue.name) or '')
+
+### COMMAND FUNCTIONS ###
+
+def run_test(user_type, **kw):
+    """
+    Enqueue a test run job with keyword arguments specified in **kw
+    """
+    queue = _get_queue(user_type=user_type, **kw)
+    _check_args(ts.run_test, **kw)
+    _print_queue_info(queue)
+    queue.enqueue_call(ats.run_test, kwargs=kw, job_id=_job_id(**kw))
+
+def update_scripts(**kw):
+    """
+    Enqueue a test script update job with keyword arguments specified in **kw
+    """
+    queue = rq.Queue(config.SERVICE_QUEUE, connection=ats._redis_connection())
+    _check_args(ts.update_test_scripts, **kw)
+    queue.enqueue_call(ats.update_test_scripts, kwargs=kw)
+
+def cancel_test(markus_address, run_ids, **kw):
+    """
+    Cancel a test run job with the job_id defined using 
+    markus_address and run_id. 
+    """
+    with rq.Connection(ats._redis_connection()):
+        for run_id in run_ids:
+            job_id = _job_id(markus_address, run_id)
+            rq.cancel_job(job_id)
+
+COMMANDS = {'run'       : run_test,
+            'scripts'   : update_scripts,
+            'cancel'    : cancel_test}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('command', choices=COMMANDS)
+    parser.add_argument('args', type=json.loads)
+
+    args = parser.parse_args()
+
+    COMMANDS[args.command](**args)
