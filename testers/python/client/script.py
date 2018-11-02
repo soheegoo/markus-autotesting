226ca8cc5cbd60f02b89af8f2889aaebafed095f
mishaschwartz <misha.schwartz@gmail.com>
2018-11-02 16:45:57 -0400

    python-tester: don't do tester discovery (use pytest) as default, allow for configurable test output for unittest and pytest

--- testers/python/client/script.py
+++ testers/python/client/script.py
@@ -29,6 +29,25 @@ if __name__ == '__main__':
     SPECS['test_points'] = {'test.py': POINTS, 'test2.py': POINTS}
 
     """
+    If using the pytest tester, set the traceback format, options are:
+        'auto', 'long', 'short', 'no', 'line', 'native'
+    The default is 'short' if commented out
+    """
+    # SPECS["pytest_tb_format"] = "short"
+
+    """
+    If using the unittest tester, set the verbosity level, options are: 0, 1, 2
+    The default is 2 if commented out
+    """
+    # SPECS["unittest_verbosity"] = 2
+
+    """
+    Choose which python tester to use, options are: 'pytest', 'unittest'
+    The default is 'pytest' if commented out
+    """
+    # SPECS["tester"] = "pytest"
+
+    """
     The feedback file name; defaults to no feedback file if commented out.
     """
     # SPECS['feedback_file'] = 'feedback_python.txt'
