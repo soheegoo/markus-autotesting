4247980c9f99eeec6d1d83209233f919836db3d5
mishaschwartz <misha.schwartz@gmail.com>
2019-02-12 10:47:36 -0500

    specs-update: update the custom tester to use new specs configuration

--- testers/testers/custom/markus_custom_tester.py
+++ testers/testers/custom/markus_custom_tester.py
@@ -1,11 +1,11 @@
 import subprocess
+import os
 
 class MarkusCustomTester:
     def __init__(self, specs):
         self.specs = specs
 
     def run(self):
-        for group in self.specs.get('runnable_group', []):
-            file_path = group.get('script_file_path')
-            if file_path:
-                subprocess.run(f'./{file_path}')
+        file_path = self.specs.get('script_file_path')
+        if file_path:
+            subprocess.run(f'./{file_path}')
