1b662256feeb38bcacc6a0dffe4fc96d77c31017
mishaschwartz <misha.schwartz@gmail.com>
2019-02-25 12:03:20 -0500

    tests: remove blank test files and update enqueuer tests

--- server/tests/autotest_enqueuer_test.py
+++ server/tests/autotest_enqueuer_test.py
@@ -92,6 +92,7 @@ class TestCheckForEnvironmentErrors:
         self.tmp_dir = tempfile.TemporaryDirectory()
         file_name = 'env_creation_errors.txt'
         self.error_file = os.path.join(self.tmp_dir.name, file_name)
+        self.specs = [{'tester_type': None, 'tester_name': None}]
         with open(self.error_file, 'w') as f:
             f.write('something')
 
@@ -99,17 +100,17 @@ class TestCheckForEnvironmentErrors:
         self.tmp_dir.cleanup()
 
     def test_reports_env_creation_error(self):
-        mock_func = 'autotest_server.get_unique_env_name'
+        mock_func = 'autotest_server.get_env_dir'
         with patch(mock_func, autospec=True, return_value=self.tmp_dir.name):
             with pytest.raises(RuntimeError) as e:
-                ate.check_for_environment_errors(None, None, None)
+                ate.check_for_environment_errors(None, self.specs)
 
     def test_no_report(self):
         os.unlink(self.error_file)
-        mock_func = 'autotest_server.get_unique_env_name'
+        mock_func = 'autotest_server.get_env_dir'
         with patch(mock_func, autospec=True, return_value=self.tmp_dir.name):
             try:
-                ate.check_for_environment_errors(None, None, None)
+                ate.check_for_environment_errors(None, self.specs)
             except RuntimeError as e:
                 pytest.fail()
 
@@ -117,10 +118,10 @@ class TestCheckForEnvironmentErrors:
     def test_report_contains_message(self, message):
         with open(self.error_file, 'w') as f:
             f.write(message)
-        mock_func = 'autotest_server.get_unique_env_name'
+        mock_func = 'autotest_server.get_env_dir'
         with patch(mock_func, autospec=True, return_value=self.tmp_dir.name):
             with pytest.raises(RuntimeError) as e:
-                ate.check_for_environment_errors(None, None, None)
+                ate.check_for_environment_errors(None, self.specs)
                 assert message in str(e)
 
 class TestQueueName:
