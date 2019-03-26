744ea0631e806abb4495619aa137a333b2c50d80
Alessio Di Sandro <alessio.disandro@gmail.com>
2019-03-26 15:03:22 +0100

    custom: Invoke super constructor

--- testers/testers/custom/markus_custom_tester.py
+++ testers/testers/custom/markus_custom_tester.py
@@ -1,9 +1,11 @@
 import subprocess
 from testers.markus_tester import MarkusTester
 
+
 class MarkusCustomTester(MarkusTester):
+
     def __init__(self, specs):
-        self.specs = specs
+        super().__init__(specs, test_class=None)
 
     def run(self):
         file_paths = self.specs.get('test_data', 'script_files', default=[])
