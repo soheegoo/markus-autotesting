8efa89b57e90f5c8e519e1a18e11b124177823be
Alessio Di Sandro <alessio.disandro@gmail.com>
2019-04-09 19:40:42 +0200

    sql: Fix installation and uninstallation scripts part 2

--- testers/testers/sql/bin/install.sh
+++ testers/testers/sql/bin/install.sh
@@ -9,16 +9,16 @@ install_packages() {
 
 create_oracle_db() {
     echo "[SQL-INSTALL] Creating oracle user '${ORACLEUSER}' with database '${ORACLEDB}'"
-    if id ${ORACLEUSER} &> /dev/null; then
-        sudo -u "${ORACLEUSER}" -- bash -c 'touch ${HOME}/.pgpass && chmod 600 ${HOME}/.pgpass'
-        read -srp "[SQL-INSTALL] Create password for oracle user '${ORACLEUSER}': " ORACLEPWD
-        echo # move to a new line
-        echo "localhost:5732:${ORACLEUSER}:${ORACLEUSER}:${ORACLEPWD}" | sudo -u ${ORACLEUSER} -- bash -c 'tee >| ${HOME}/.pgpass'
-    else
+    if ! id ${ORACLEUSER} &> /dev/null; then
         echo "[SQL-INSTALL] ${ORACLEUSER} must be a valid user on this machine" >&2
         exit 1
     fi
 
+    sudo -u "${ORACLEUSER}" -- bash -c 'touch ${HOME}/.pgpass && chmod 600 ${HOME}/.pgpass'
+    read -srp "[SQL-INSTALL] Create password for oracle user '${ORACLEUSER}': " ORACLEPWD # s: no echo, r: no backslash escape
+    echo # move to a new line
+    echo "localhost:5432:${ORACLEUSER}:${ORACLEUSER}:${ORACLEPWD}" | sudo -u ${ORACLEUSER} -- bash -c 'tee >| ${HOME}/.pgpass'
+    INSTALL_SETTINGS="{\"oracle_database\": \"${ORACLEDB}\"}"
     sudo -u postgres psql <<-EOF
 		CREATE USER ${ORACLEUSER} WITH PASSWORD '${ORACLEPWD}';
 		CREATE DATABASE ${ORACLEDB} OWNER ${ORACLEUSER};
@@ -32,35 +32,34 @@ random_pwd() {
     date +%s%N | sha256sum | base64 | head -c 32
 }
 
-create_test_dbs() {
-    local i=0
-    while read -r tests_json; do
-        local user=$(echo ${tests_json} | jq --raw-output .user)
-        if [[ "${user}" != "${ORACLEUSER}" ]]; then
-            local database=$(echo ${tests_json} | jq --raw-output .database)
-            echo "[SQL-INSTALL] Creating test user '${user}' with database '${database}'"
-            local test_pwd=$(random_pwd)
-            sudo -u postgres psql <<-EOF
-				CREATE USER ${user} WITH PASSWORD '${test_pwd}';
-				CREATE DATABASE ${database} OWNER ${user};
-				REVOKE CONNECT ON DATABASE ${database} FROM PUBLIC;
-				GRANT CONNECT ON DATABASE ${ORACLEDB} TO ${user};
-				\connect ${database}
-				DROP SCHEMA IF EXISTS public CASCADE;
-			EOF
-        else
-            local test_pwd=$(grep -Po "(?<=${ORACLEUSER}:)(.*)$" ${HOME}/.pgpass)
-        fi
-        INSTALL_SETTINGS=$(echo ${INSTALL_SETTINGS} | jq ".tests[${i}].password=\"${test_pwd}\"")
-        ((i++))
-    done < <(echo ${INSTALL_SETTINGS} | jq --compact-output '.tests | .[]')
-    echo ${INSTALL_SETTINGS}
+add_test_specs_settings() {
+    local db=$1
+    local user=$2
+    local pwd=$3
+    local test_str="{\"database\": \"${db}\", \"user\": \"${user}\", \"password\": \"${pwd}\"}"
+    INSTALL_SETTINGS=$(echo ${INSTALL_SETTINGS} | jq ".tests += [${test_str}]")
 }
 
-add_db_user_settings() {
-    local user_name=$1
-    local test_str="{\"database\": \"${user_name}\", \"user\": \"${user_name}\"}"
-    INSTALL_SETTINGS=$(echo ${INSTALL_SETTINGS} | jq ".tests += [${test_str}]")
+create_test_dbs() {
+    if [[ -z ${TESTUSERS} ]]; then
+        echo "[SQL-INSTALL] Reusing oracle user '${ORACLEUSER}' with database '${ORACLEDB}' as test user"
+        add_test_specs_settings ${ORACLEUSER} ${ORACLEDB} ${ORACLEPWD}
+    else
+        for test_user in ${TESTUSERS}; do
+            local test_db=${test_user}
+            local test_pwd=$(random_pwd)
+            echo "[SQL-INSTALL] Creating test user '${test_user}' with database '${test_db}'"
+            add_test_specs_settings ${test_db} ${test_user} ${test_pwd}
+            sudo -u postgres psql <<-EOF
+				CREATE USER ${test_user} WITH PASSWORD '${test_pwd}';
+				CREATE DATABASE ${test_db} OWNER ${test_user};
+				REVOKE CONNECT ON DATABASE ${test_db} FROM PUBLIC;
+				GRANT CONNECT ON DATABASE ${ORACLEDB} TO ${test_user};
+				\connect ${test_db}
+				DROP SCHEMA IF EXISTS public CASCADE;
+			EOF
+        done
+    fi
 }
 
 create_specs() {
@@ -90,19 +89,10 @@ if [[ -z ${ORACLEUSER} ]]; then
     ORACLEUSER=$(whoami)
 fi
 ORACLEDB=${ORACLEUSER}
-INSTALL_SETTINGS="{\"oracle_database\": \"${ORACLEDB}\"}"
-
-if [[ -z ${TESTUSERS} ]]; then
-    add_db_user_settings ${ORACLEUSER}
-else
-    for test_user in ${TESTUSERS}; do
-        add_db_user_settings ${test_user}
-    done
-fi
 
 # main
 install_packages
 create_oracle_db
-INSTALL_SETTINGS=$(create_test_dbs)
+create_test_dbs
 create_specs
 touch ${SPECSDIR}/.installed
--- testers/testers/sql/bin/uninstall.sh
+++ testers/testers/sql/bin/uninstall.sh
@@ -6,16 +6,18 @@ drop_oracle_db() {
 		DROP DATABASE IF EXISTS ${ORACLEDB};
 		DROP ROLE IF EXISTS ${ORACLEUSER};
 	EOF
+    sudo -u ${ORACLEUSER} -- bash -c 'rm -f ${HOME}/.pgpass'
 }
 
 drop_test_dbs() {
-    while read -r tester; do
-        echo "[SQL-UNINSTALL] Removing test user '${tester}' with database '${tester}'"
+    for test_user in ${TESTUSERS}; do
+        local test_db=${test_user}
+        echo "[SQL-UNINSTALL] Removing test user '${test_user}' with database '${test_db}'"
         sudo -u postgres psql <<-EOF
-			DROP DATABASE IF EXISTS ${tester};
-			DROP ROLE IF EXISTS ${tester};
+			DROP DATABASE IF EXISTS ${test_db};
+			DROP ROLE IF EXISTS ${test_user};
 		EOF
-    done < <(echo ${INSTALLSETTINGS} | jq --raw-output '.tests | .[] | .user')
+    done
 }
 
 reset_specs() {
@@ -33,9 +35,10 @@ fi
 THISSCRIPT=$(readlink -f ${BASH_SOURCE})
 TESTERDIR=$(dirname $(dirname ${THISSCRIPT}))
 SPECSDIR=${TESTERDIR}/specs
-INSTALLSETTINGS=$(cat ${SPECSDIR}/install_settings.json | jq .)
+INSTALLSETTINGS=$(cat ${SPECSDIR}/install_settings.json)
 ORACLEDB=$(echo ${INSTALLSETTINGS} | jq --raw-output .oracle_database)
 ORACLEUSER=${ORACLEDB}
+TESTUSERS=$(echo ${INSTALLSETTINGS} | jq --raw-output '.tests | .[] | .user')
 
 # main
 drop_oracle_db
