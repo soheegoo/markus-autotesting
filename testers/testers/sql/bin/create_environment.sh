34838b7c78b218d764baeb89a404234439755ba5
Alessio Di Sandro <alessio.disandro@gmail.com>
2019-05-22 22:17:04 +0200

    sql: Fix env scripts part 2

--- testers/testers/sql/bin/create_environment.sh
+++ testers/testers/sql/bin/create_environment.sh
@@ -21,71 +21,62 @@ get_all_test_users() {
     echo ${SETTINGS_JSON} | jq --raw-output '.install_data | .tests | .[] | .user' | head -c -1 | tr '\n' ','
 }
 
-get_test_data() {
-    echo ${SETTINGS_JSON} | 
-        jq --compact-output '
-            .test_data |
-            .[] |
-            .query_files |
-            .[] |
-            {query_file: .query_file, data_files: .data_files}'
-}
-
-create_schema_str() {
+create_schema_sql() {
     local schema_name=$1
     local data_file=${SOLUTION_DIR}/$2
-    local schema_file_path=$(echo ${SETTINGS_JSON} | jq --raw-output .env_data.schema_file_path)
-    local schema_file_abs_path="${SOLUTION_DIR}/${schema_file_path}"
+    local schema_file="${SOLUTION_DIR}/$(echo ${SETTINGS_JSON} | jq --raw-output .env_data.schema_file_path)"
     local all_test_users=$(get_all_test_users)
     echo "
         DROP SCHEMA IF EXISTS ${schema_name} CASCADE;
         CREATE SCHEMA ${schema_name};
         GRANT USAGE ON SCHEMA ${schema_name} TO ${all_test_users};
         SET search_path TO ${schema_name};
-    " | cat - ${schema_file_abs_path} ${data_file}
+    " | cat - ${schema_file} ${data_file}
 }
 
-create_solution_str() {
+create_solution_sql() {
     local schema_name=$1
     local query_file=${SOLUTION_DIR}/$2
     local query_name=$(basename -s .sql ${query_file})
     local all_test_users=$(get_all_test_users)
     echo "
         SET search_path TO ${schema_name};
-    " | cat - ${query_file} <(echo "GRANT SELECT ON ${query_name} TO ${all_test_users};")
+    " | cat - ${query_file} <(echo "GRANT SELECT ON ${schema_name}.${query_name} TO ${all_test_users};")
 }
 
-create_db_load_str() {
+create_sql() {
     local schemas=""
     local tester_name=$(basename ${ENV_DIR})
-    while read -r files_json; do
-        local query_file=$(echo ${files_json} | jq --raw-output .query_file)
-        local data_files=$(echo ${files_json} | jq --raw-output '.data_files | .[]')
-        while read -r data_file; do
+    local files_json=$(echo ${SETTINGS_JSON} | jq --compact-output '.test_data | .[] | .query_files | .[] |
+                                                                    {query_file: .query_file, data_files: .data_files}')
+    for entry_json in ${files_json}; do
+        local query_file=$(echo ${entry_json} | jq --raw-output .query_file)
+        local data_files=$(echo ${entry_json} | jq --raw-output '.data_files | .[]')
+        for data_file in ${data_files}; do
             local schema_name="${tester_name}_$(basename -s .sql ${data_file})"
-            if [[ ${schemas} != *" ${schema_name} "* ]]; then # first time using this dataset, create a schema for it
-                local schema_string=$(create_schema_str ${schema_name} ${data_file})
-                local schemas="${schemas} ${schema_name} "
-            else
-                local schema_string=''
+            local schema_sql=""
+            if [[ "${schemas}" != *" ${schema_name} "* ]]; then # first time using this dataset, create a schema for it
+                schema_sql=$(create_schema_sql ${schema_name} ${data_file})
+                schemas="${schemas} ${schema_name} "
             fi
-            local solution_string=$(create_solution_str ${schema_name} ${query_file})
-            echo "${schema_string} ${solution_string}" 
-        done < <(echo "${data_files}")
-    done < <(get_test_data)
+            local solution_sql=$(create_solution_sql ${schema_name} ${query_file})
+            echo "${schema_sql} ${solution_sql}"
+        done
+    done
 }
 
-load_solutions_to_db() {
+load_solution() {
     local oracle_db=$(echo ${SETTINGS_JSON} | jq --raw-output .install_data.oracle_database)
     local oracle_user=$(whoami)
-    echo $(create_db_load_str) > /home/vagrant/markus-autotesting/tmp.log
-    psql -U ${oracle_user} -d ${oracle_db} -h localhost -f <(echo $(create_db_load_str))
+    local temp_log=/tmp/tmp.sql
+    echo $(create_sql) >| ${temp_log}
+    psql -U ${oracle_user} -d ${oracle_db} -h localhost -f ${temp_log}
 }
 
-clean_solutions_dir() {
+clean_files() {
     local query_files=$(echo ${SETTINGS_JSON} | jq --raw-output '.test_data | .[] | .query_files | .[] | .query_file')
-    echo "${query_files}" | while read -r query_file; do
-        rm "${SOLUTION_DIR}/${query_file}"
+    for query_file in ${query_files}; do
+        rm -f "${SOLUTION_DIR}/${query_file}"
     done
 }
 
@@ -100,11 +91,10 @@ SETTINGS_JSON=$1
 FILES_DIR=$(readlink -f $2)
 
 ENV_DIR=$(echo ${SETTINGS_JSON} | jq --raw-output .env_loc)
-
 VENV_DIR=${ENV_DIR}/venv
+SOLUTION_DIR=${ENV_DIR}/solution
 PY_VERSION=3.7
 PIP_REQUIREMENTS='psycopg2-binary'
-SOLUTION_DIR=${ENV_DIR}/solution
 
 THIS_SCRIPT=$(readlink -f ${BASH_SOURCE})
 THIS_DIR=$(dirname ${THIS_SCRIPT})
@@ -113,6 +103,5 @@ TESTERS_DIR=$(readlink -f ${THIS_DIR}/../../../)
 # main
 create_venv
 move_files
-load_solutions_to_db
-clean_solutions_dir
-
+load_solution
+clean_files
