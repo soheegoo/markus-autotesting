8f3cb184c3a368dd16766d74df89df2a44a17a74
mishaschwartz <misha.schwartz@gmail.com>
2022-09-27 15:08:31 -0400

    flask: update configurations for flask settings (#400)

--- client/run.py
+++ client/run.py
@@ -2,4 +2,4 @@ import os
 from autotest_client import app
 
 if __name__ == "__main__":
-    app.run(host=os.environ["FLASK_HOST"], port=int(os.environ["FLASK_PORT"]))
+    app.run(host=os.environ["FLASK_HOST"], port=int(os.environ["FLASK_PORT"]), debug=True)
