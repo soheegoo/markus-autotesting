ee4ceddcdff41ec4ba76b6c511c0f840b572222f
Sohee Goo <soheegoo@gmail.com>
2025-01-14 10:40:36 -0500

    allow renv.lock file upload and manual listing of R dependencies

--- server/autotest_server/testers/r/lib/r_renv_setup.R
+++ server/autotest_server/testers/r/lib/r_renv_setup.R
@@ -6,32 +6,13 @@ if (!("renv" %in% installed.packages())) {
 }
 library(renv)
 
-# Tester dependencies
-# rjson v0.2.20 is required to support R v3.x
-deps <- c("testthat", "rjson", "stringi", "tidyverse")
-
-# Install the required packages if they are not already installed
-installed_deps <- rownames(installed.packages())
-missing_deps <- deps[!deps %in% installed_deps]
-if (length(missing_deps) > 0) {
-  install.packages(missing_deps)
-}
-# Make sure rjson v0.2.20 is installed
-if (!("rjson" %in% installed.packages()[,"Package"]) || 
-    packageVersion("rjson") != "0.2.20") {
-  install.packages("rjson", version = "0.2.20")
-}
-
 # Set the path for the renv.lock file and the env_dir directory
 lockfile <- commandArgs(trailingOnly = TRUE)[1]
 env_dir <- commandArgs(trailingOnly = TRUE)[2]
 
-# Check if renv.lock file exists
 if (!file.exists(lockfile)) {
   stop("renv.lock file not found: ", lockfile)
 }
 
 # Initialize the renv environment and restore dependencies from the lockfile
-renv::restore(lockfile = lockfile, lib = env_dir)
-
-print("R environment setup complete.\n")
+renv::restore(lockfile = lockfile, library = env_dir)
--- server/autotest_server/testers/r/setup.py
+++ server/autotest_server/testers/r/setup.py
@@ -2,27 +2,25 @@ import os
 import json
 import subprocess
 
-
 def create_environment(settings_, env_dir, default_env_dir):
     env_data = settings_.get("env_data", {})
-    renv_bool = env_data.get("requirements", False)
+    lockfile_submitted = env_data.get("renv.lock", False)
     os.makedirs(env_dir, exist_ok=True)
     env = {"R_LIBS_SITE": env_dir, "R_LIBS_USER": env_dir}
-
-    #r_tester_setup = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib", "r_tester_setup.R")
-    # subprocess.run(
-    #     ["Rscript", r_tester_setup, req_string],
-    #     env={**os.environ, **env},
-    #     check=True,
-    #     text=True,
-    #     capture_output=True,
-    # )
     
-    r_renv_setup = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib", "r_renv_setup.R")
-    
-    if renv_bool:
-        renv_lock_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "renv.lock")
-
+    req_string = env_data.get("requirements", "")
+    r_tester_setup = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib", "r_tester_setup.R")
+    subprocess.run(
+        ["Rscript", r_tester_setup, req_string],
+        env={**os.environ, **env},
+        check=True,
+        text=True,
+        capture_output=True,
+    )
+        
+    if lockfile_submitted:
+        renv_lock_path = os.path.join(env_dir, "../", "files", "renv.lock")
+        r_renv_setup = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib", "r_renv_setup.R")
         if os.path.exists(renv_lock_path):
             subprocess.run(
                 ["Rscript", r_renv_setup, renv_lock_path, env_dir],
@@ -31,9 +29,6 @@ def create_environment(settings_, env_dir, default_env_dir):
                 text=True,
                 capture_output=True,
             )
-        else:
-            raise FileNotFoundError(f"renv.lock file not found in {os.path.dirname(os.path.realpath(__file__))}")
-
     return {**env, "PYTHON": os.path.join(default_env_dir, "bin", "python3")}
 
 
